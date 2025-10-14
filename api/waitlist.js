39368573673a5e047585eeba31b689a7b7c95c00
Moyoscooo <moyosorejames@gmail.com>
2025-10-14 16:43:20 +0100

    push wait

--- api/waitlist.js
+++ api/waitlist.js
@@ -266,6 +266,6 @@ export default async function handler(req, res) {
     return res.status(200).json({ success: true, data: response.data });
   } catch (error) {
     console.error(error.response?.data || error.message);
-    return res.status(500).json({ error: "Failed to send email" });
+    return res.status(500).json({ error: "Failed to send email", message: error.message });
   }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,14 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // Firebase configuration using environment variables
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAeeEh4DfPg0xAaPtDqE726ds-OkheUKZ0",
-  authDomain: "osondu-ba2a6.firebaseapp.com",
-  projectId: "osondu-ba2a6",
-  storageBucket: "osondu-ba2a6.firebasestorage.app",
-  messagingSenderId: "813833532483",
-  appId: "1:813833532483:web:2fe5d1ff391489cbca90f6",
-  measurementId: "G-WCJMYK2RF7",
+  apiKey: "AIzaSyDJGb43_z_RSQ3guah9V1Yx7-CJsaCu7b4",
+  authDomain: "yagso-89499.firebaseapp.com",
+  projectId: "yagso-89499",
+  storageBucket: "yagso-89499.firebasestorage.app",
+  messagingSenderId: "339377343543",
+  appId: "1:339377343543:web:97a12e0658d1c42cb443b2",
+  measurementId: "G-0FGZKE1XGH",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
