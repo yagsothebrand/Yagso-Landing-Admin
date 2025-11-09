3213c4c763b3cab26da714e6e949a9c34faab351
Moyoscooo <moyosorejames@gmail.com>
2025-11-09 22:08:16 +0100

    push chunk

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,22 @@ import { LoadingHelper } from "./lib/LoadingHelper";
 import { ProtectedRoute } from "./components/auth/protected-route";
 
 // Public pages
-import Index from "./pages/admin/Index";
-import NotFound from "./pages/NotFound";
-
-// Protected pages
-import AdminDashboard from "./components/layout/AdminDashboard";
-import InvoicesPage from "./pages/InvoicesPage";
-import { CategoryPage } from "./pages/CategoryPage";
-import { AdministrationPage } from "./pages/AdministrationPage";
-import { AnalyticsDashboard } from "./pages/AnalyticsDashboard";
-import { EmailLogsPage } from "./pages/EmailLogsPage";
-import ComingSoon from "./pages/ComingSoon";
-import WaitlistForm from "./pages/landing/WaitListForm";
-import Page from "./pages/landing/Index";
-import { LandingAuthProvider } from "./components/landingauth/LandingAuthProvider";
-import { ProductsPage } from "./pages/ProductsPage";
-import { WaitListEmails } from "./pages/WaitListEmails";
-import ProductDetails from "./pages/landing/ProductDetails";
-import Layout from "./components/layouts/Layout";
-import Home from "./pages/landing/Home";
-
+const Index = lazy(() => import("./pages/admin/Index"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const AdminDashboard = lazy(() => import("./components/layout/AdminDashboard"));
+const InvoicesPage = lazy(() => import("./pages/InvoicesPage"));
+const CategoryPage = lazy(() => import("./pages/CategoryPage"));
+const AdministrationPage = lazy(() => import("./pages/AdministrationPage"));
+const AnalyticsDashboard = lazy(() => import("./pages/AnalyticsDashboard"));
+const EmailLogsPage = lazy(() => import("./pages/EmailLogsPage"));
+const ComingSoon = lazy(() => import("./pages/ComingSoon"));
+const WaitlistForm = lazy(() => import("./pages/landing/WaitListForm"));
+const Page = lazy(() => import("./pages/landing/Index"));
+const ProductsPage = lazy(() => import("./pages/ProductsPage"));
+const WaitListEmails = lazy(() => import("./pages/WaitListEmails"));
+const ProductDetails = lazy(() => import("./pages/landing/ProductDetails"));
+const Layout = lazy(() => import("./components/layouts/Layout"));
+const Home = lazy(() => import("./pages/landing/Home"));
 function App() {
   const router = createBrowserRouter([
     {
