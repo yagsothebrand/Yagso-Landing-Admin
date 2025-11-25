7d166576c244349952b745fe7c9b3922a407f7c3
Moyo <moyosorejames@gmail.com>
2025-11-25 23:42:43 +0100

    really fix

--- src/pages/landing/WaitlistHome.jsx
+++ src/pages/landing/WaitlistHome.jsx
@@ -139,7 +139,7 @@ export default function HomePage() {
       ) : null}
 
       <AnimatePresence>
-        {showForm && (
+        {showForm && !success && (
           <EmailForm
             onSubmit={handleEmailSubmit}
             loading={loading}
